4faa2b988d73250cfc9422f58beb9de5a9aeb8e9
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-21 13:08:43 +0300

    classes created and Task.java coded

---
+++ src/main/java/org/example/entity/Task.java
@@ -0,0 +1,52 @@
+package org.example.entity;
+
+import java.util.Objects;
+
+public class Task {
+    private String project;
+    private String description;
+    private String assignee;
+    private Priority priority;
+    private Status status;
+
+    public Task(String project, String description, String assignee, Status status, Priority priority) {
+        this.project = project;
+        this.description = description;
+        this.assignee = assignee;
+        this.status = status;
+        this.priority = priority;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Task task)) return false;
+        return Objects.equals(project, task.project) &&
+                Objects.equals(description, task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(project, description);
+    }
+}
