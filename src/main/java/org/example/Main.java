a24d6663f8e3e1f31a2197de8edf5d68d30e5789
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-21 13:15:41 +0300

    Main.java done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.entity.StringSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Main çalışıyor.");
+        System.out.println("Unique words: " + StringSet.findUniqueWords().size());
+        System.out.println("İlk 10 kelime:");
+        StringSet.findUniqueWords().stream().limit(10).forEach(System.out::println);
     }
 }
